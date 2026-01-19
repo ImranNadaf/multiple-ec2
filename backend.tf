5397771d2b3e23c25047af7e7410110448f16597
ImranNadaf <imrannadaf472@gmail.com>
2026-01-19 16:57:39 +0530

    new change

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "imran-terraform-state-us-east-2-2026"
-    key            = "multiple-ec2/terraform.tfstate"
-    region         = "us-east-2"
-    encrypt        = true
+    bucket       = "imran-terraform-state-us-east-2-2026"
+    key          = "multiple-ec2/terraform.tfstate"
+    region       = "us-east-2"
+    encrypt      = true
     use_lockfile = true
   }
 }
